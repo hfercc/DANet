d69104cb3fbd6e594ebbd74245da30ccb984b1e5
ChanYH <hfercxx@hotmail.com>
2019-01-23 17:48:43 +0800

    p

--- depth/enc/nn/customize.py
+++ depth/enc/nn/customize.py
@@ -102,7 +102,7 @@ class SegmentationMultiLosses(CrossEntropyLoss):
     def forward(self, *inputs): 
 
         *preds, target = tuple(inputs)
-        pred1 = tuple(preds)[0][0]
+        pred1 = tuple(preds)[0]
         print(pred1.shape)
         print(target.shape)
         loss1 = super(SegmentationMultiLosses, self).forward(pred1, target)
