e6616b067de971e1ac5cf9db7915311421125fe6
ChanYH <hfercxx@hotmail.com>
2019-02-23 12:07:25 +0800

    p

--- depth/train.py
+++ depth/train.py
@@ -145,7 +145,7 @@ class Trainer():
             #print(outputs[0][0].shape)
             #print(target.shape)
             depth = depth.cuda()
-            depth = fcrn(depth)
+            depth = fcrn(depth).unsqueeze(1)
             if i % 10 == 0:
                 loss = self.criterion(outputs, depth, target.float(), (i%10==0))
             else:
