5b9d779bb3b75079523af634e022766f9cb2826a
ChanYH <hfercxx@hotmail.com>
2019-02-23 11:08:31 +0800

    p

--- depth/train.py
+++ depth/train.py
@@ -135,7 +135,7 @@ class Trainer():
             #print(outputs)
             #print(outputs[0][0].shape)
             #print(target.shape)
-            loss = self.criterion(outputs, target, depth, rev)
+            loss = self.criterion(outputs, target, depth, (i%10==0))
             loss.backward()
             #loss = self.criterion(outputs, 1, depth)
             #loss.backward()
