ec054804d1712c99a55c0e8111006b9648819a09
hfercc <hfercxx@hotmail.com>
2019-04-09 10:38:45 +0800

    p

--- encoding/nn/customize.py
+++ encoding/nn/customize.py
@@ -97,6 +97,7 @@ class SegmentationMultiLosses(CrossEntropyLoss):
     def forward(self, *inputs):
 
         *preds, _, target = tuple(inputs)
+        print(preds)
         pred1, pred2 ,pred3= tuple(preds[0])
 
 
