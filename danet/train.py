c39a3104e03a8f946d8aa1f4d33a8bcefb56a53e
hfercc <hfercxx@hotmail.com>
2019-04-09 19:03:49 +0800

    p

--- danet/train.py
+++ danet/train.py
@@ -161,10 +161,8 @@ class Trainer():
         # Fast test during the training
         def eval_batch(model, image, target):
             outputs = model(image)
-            print(outputs)
-            if self.args.sing:
-                preds = tuple(outputs[0],)
-                outputs = tuple(preds)
+            preds = tuple(outputs[0],)
+            outputs = tuple(preds)
             outputs = gather(outputs, 0, dim=0)
             pred = outputs[0].unsqueeze(0)
             target = target.cuda().unsqueeze(0)
