d212ad947e36bcfb2aa7d4a84488c45cb72f50c7
hfercc <hfercxx@hotmail.com>
2019-04-16 11:09:27 +0800

    p

--- danet/train.py
+++ danet/train.py
@@ -132,7 +132,6 @@ class Trainer():
                 preds = [tuple(i[0],) for i in outputs]
                 feature_dict = [(i[1],) for i in outputs]
                 feature_dict = tuple(feature_dict)
-                print(feature_dict)
                 loss = self.criterion(preds, target)
                 singular_loss = self.singular_loss(feature_dict, target)
                 #loss += singular_loss
